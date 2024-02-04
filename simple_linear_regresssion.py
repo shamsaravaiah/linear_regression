6a25fb1007f30d22e09a33193c6183e35aec5bf5
shamsaravaiah <sham.saravai@gmail.com>
2024-02-04 13:44:34 +0100

    random comment

--- simple_linear_regresssion.py
+++ simple_linear_regresssion.py
@@ -1,6 +1,9 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
 
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
@@ -23,16 +26,25 @@ y_pred = regressor.predict(X_test)
 
 #visualising
 
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# Training set visualization
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
+
+# Display the first plot
 plt.show()
 
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# Test set visualization
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_test, regressor.predict(X_test), color='blue')  
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
+
+# Display the second plot
 plt.show()
+
+
+#hi 
